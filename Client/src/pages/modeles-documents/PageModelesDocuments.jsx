67b0eb797678149678e7259c07358a1a49b96932
Amadiane <129948408+Amadiane@users.noreply.github.com>
2026-08-26 09:37:48 +0100

    corrige5

--- Client/src/pages/modeles-documents/PageModelesDocuments.jsx
+++ Client/src/pages/modeles-documents/PageModelesDocuments.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import { useTranslation } from "react-i18next";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
 import { documentsGeneresService } from "../../services/documentsGeneresService";
 import styles from "../../theme/pages/modeles-documents/PageModelesDocuments.module.css";
 
