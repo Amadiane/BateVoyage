a999b19c829a7dea28c7344d831f94a76b3d52ca
Amadiane <129948408+Amadiane@users.noreply.github.com>
2026-09-01 15:44:38 +0100

    vol add2

--- Server/groupes_vols/views.py
+++ Server/groupes_vols/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+from django.contrib.contenttypes.models import ContentType
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from auditlog.context import set_actor
+from auditlog.models import LogEntry
 from xhtml2pdf import pisa
 
+from pelerins.models import Pelerin
 from pelerins.pdf_utils import link_callback
+from activite.serializers import EntreeJournalDetailSerializer
 from utilisateurs.permissions import EstGestionnaireLogistique
 from .models import Vol, Groupe
 from .serializers import VolSerializer, GroupeSerializer
@@ -31,6 +32,24 @@ class VolViewSet(viewsets.ModelViewSet):
     def perform_destroy(self, instance):
         with set_actor(self.request.user):
             instance.delete()
+
+    @action(detail=True, methods=["get"], url_path="manifeste-pdf")
+    def manifeste_pdf(self, request, pk=None):
+        vol = self.get_object()
+        pelerins_aller = Pelerin.objects.filter(groupe__vol_aller=vol)
+        pelerins_retour = Pelerin.objects.filter(groupe__vol_retour=vol)
+
+        html = render_to_string("groupes_vols/manifeste_vol.html", {
+            "v": vol, "pelerins_aller": pelerins_aller, "pelerins_retour": pelerins_retour,
+        })
+
+        response = HttpResponse(content_type="application/pdf")
+        response["Content-Disposition"] = f'attachment; filename="manifeste_vol_{vol.numero_vol}.pdf"'
+
+        resultat = pisa.CreatePDF(html, dest=response, link_callback=link_callback)
+        if resultat.err:
+            return Response({"erreur": "Échec de la génération du PDF."}, status=500)
+        return response
 
 
 class GroupeViewSet(viewsets.ModelViewSet):
@@ -67,7 +86,6 @@ class GroupeViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=["post"], url_path="affecter-pelerins")
     def affecter_pelerins(self, request, pk=None):
-        from pelerins.models import Pelerin
         groupe = self.get_object()
         ids = request.data.get("pelerin_ids", [])
         with set_actor(request.user):
@@ -76,32 +94,7 @@ class GroupeViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=["post"], url_path="retirer-pelerin")
     def retirer_pelerin(self, request, pk=None):
-        from pelerins.models import Pelerin
         pelerin_id = request.data.get("pelerin_id")
         with set_actor(request.user):
             Pelerin.objects.filter(id=pelerin_id, groupe_id=pk).update(groupe=None)
         return Response({"detail": "Pèlerin retiré du groupe."})
-
-    @action(detail=True, methods=["get"], url_path="manifeste-pdf")
-    def manifeste_pdf(self, request, pk=None):
-        from pelerins.models import Pelerin
-        from django.template.loader import render_to_string
-        from django.http import HttpResponse
-        from xhtml2pdf import pisa
-        from pelerins.pdf_utils import link_callback
-
-        vol = self.get_object()
-        pelerins_aller = Pelerin.objects.filter(groupe__vol_aller=vol)
-        pelerins_retour = Pelerin.objects.filter(groupe__vol_retour=vol)
-
-        html = render_to_string("groupes_vols/manifeste_vol.html", {
-            "v": vol, "pelerins_aller": pelerins_aller, "pelerins_retour": pelerins_retour,
-        })
-
-        response = HttpResponse(content_type="application/pdf")
-        response["Content-Disposition"] = f'attachment; filename="manifeste_vol_{vol.numero_vol}.pdf"'
-
-        resultat = pisa.CreatePDF(html, dest=response, link_callback=link_callback)
-        if resultat.err:
-            return Response({"erreur": "Échec de la génération du PDF."}, status=500)
-        return response
