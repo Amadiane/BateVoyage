ef33a14937f9163c6b2fb438e581b16bcdbeabc1
Amadiane <129948408+Amadiane@users.noreply.github.com>
2026-09-08 14:58:42 +0100

    hhh11

--- Server/documents/views.py
+++ Server/documents/views.py
@@ -20,7 +20,7 @@ class TableauBordDocumentsView(APIView):
         aujourdhui = date.today()
         seuil = aujourdhui + timedelta(days=SEUIL_EXPIRATION_JOURS)
 
-        pelerins = Pelerin.objects.exclude(statut=Pelerin.Statut.CLOTURE).exclude(programme__est_archive=True).select_related("inscripteur")
+        pelerins = Pelerin.objects.select_related("programme", "groupe", "chambre").all()
 
         dossiers_incomplets = []
         passeports_expirant = []
